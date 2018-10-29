dc3b1efb01f4b6af971163566d96f7793a254138
Daniel LANGRIDGE <8DLA1005@barton.ac.uk>
2018-10-29 18:05:03 +0000

    Added btn's

--- Inventory Management System/Form1.vb
+++ Inventory Management System/Form1.vb
@@ -2,4 +2,12 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'This is a comment
     End Sub
+
+    Private Sub Btn_Button_Click(sender As Object, e As EventArgs) Handles Btn_Button.Click
+        MsgBox("Also a bit shit if im honest")
+    End Sub
+
+    Private Sub Btn_Trap_Click(sender As Object, e As EventArgs) Handles Btn_Trap.Click
+        MsgBox("Bit shit really")
+    End Sub
 End Class
