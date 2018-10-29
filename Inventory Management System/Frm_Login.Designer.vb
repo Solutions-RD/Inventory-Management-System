21e0255ce16b83ff6437373a9f758c43089247d9
Daniel LANGRIDGE <8DLA1005@barton.ac.uk>
2018-10-29 19:55:24 +0000

    Added a test comment from PC

--- Inventory Management System/Frm_Login.Designer.vb
+++ Inventory Management System/Frm_Login.Designer.vb
@@ -31,56 +31,62 @@ Partial Class Frm_Login
         '
         'TxtBox_UserName
         '
-        Me.TxtBox_UserName.Location = New System.Drawing.Point(12, 44)
+        Me.TxtBox_UserName.Location = New System.Drawing.Point(5, 20)
+        Me.TxtBox_UserName.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.TxtBox_UserName.Name = "TxtBox_UserName"
-        Me.TxtBox_UserName.Size = New System.Drawing.Size(475, 35)
+        Me.TxtBox_UserName.Size = New System.Drawing.Size(206, 20)
         Me.TxtBox_UserName.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Location = New System.Drawing.Point(5, 4)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(124, 29)
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Username"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 101)
+        Me.Label2.Location = New System.Drawing.Point(5, 45)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(124, 29)
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Username"
         '
         'TxtBox_Password
         '
-        Me.TxtBox_Password.Location = New System.Drawing.Point(12, 136)
+        Me.TxtBox_Password.Location = New System.Drawing.Point(5, 61)
+        Me.TxtBox_Password.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.TxtBox_Password.Name = "TxtBox_Password"
         Me.TxtBox_Password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TxtBox_Password.Size = New System.Drawing.Size(475, 35)
+        Me.TxtBox_Password.Size = New System.Drawing.Size(206, 20)
         Me.TxtBox_Password.TabIndex = 2
         '
         'Btn_Login
         '
-        Me.Btn_Login.Location = New System.Drawing.Point(17, 200)
+        Me.Btn_Login.Location = New System.Drawing.Point(7, 90)
+        Me.Btn_Login.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.Btn_Login.Name = "Btn_Login"
-        Me.Btn_Login.Size = New System.Drawing.Size(221, 56)
+        Me.Btn_Login.Size = New System.Drawing.Size(95, 25)
         Me.Btn_Login.TabIndex = 4
         Me.Btn_Login.Text = "Login"
         Me.Btn_Login.UseVisualStyleBackColor = True
         '
         'Frm_Login
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(14.0!, 29.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(524, 277)
+        Me.ClientSize = New System.Drawing.Size(225, 124)
         Me.Controls.Add(Me.Btn_Login)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TxtBox_Password)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TxtBox_UserName)
+        Me.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.Name = "Frm_Login"
         Me.Text = "Frm_Login"
         Me.ResumeLayout(False)
--- Inventory Management System/Frm_Login.vb
+++ Inventory Management System/Frm_Login.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Frm_Login
-
+    Private Sub Frm_Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'This is also a Comment
+    End Sub
 End Class
