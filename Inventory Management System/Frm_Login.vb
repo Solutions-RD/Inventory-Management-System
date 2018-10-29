3809e13128bcd512edc30d062d0d3b270f6c54d0
Daniel Langridge <daniel.langridge1@outlook.com>
2018-10-29 21:47:31 +0000

    Added Test Function from laptop

--- Inventory Management System/Frm_Login.vb
+++ Inventory Management System/Frm_Login.vb
@@ -2,4 +2,9 @@
     Private Sub Frm_Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'I have removed the comment
     End Sub
+
+    Public Sub Test()
+
+
+    End Sub
 End Class
