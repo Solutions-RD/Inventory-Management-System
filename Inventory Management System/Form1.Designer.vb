68ed69939b637c0cfd8e9ddc817a7e4d0b0e8a0d
Daniel LANGRIDGE <8DLA1005@barton.ac.uk>
2018-10-29 18:06:21 +0000

    Revert "Added btn's"
    This reverts commit dc3b1efb01f4b6af971163566d96f7793a254138.

--- Inventory Management System/Form1.Designer.vb
+++ Inventory Management System/Form1.Designer.vb
@@ -22,41 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Btn_Trap = New System.Windows.Forms.Button()
-        Me.Btn_Button = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'Btn_Trap
-        '
-        Me.Btn_Trap.Location = New System.Drawing.Point(62, 64)
-        Me.Btn_Trap.Name = "Btn_Trap"
-        Me.Btn_Trap.Size = New System.Drawing.Size(176, 23)
-        Me.Btn_Trap.TabIndex = 0
-        Me.Btn_Trap.Text = "This is a trap"
-        Me.Btn_Trap.UseVisualStyleBackColor = True
-        '
-        'Btn_Button
-        '
-        Me.Btn_Button.Location = New System.Drawing.Point(62, 35)
-        Me.Btn_Button.Name = "Btn_Button"
-        Me.Btn_Button.Size = New System.Drawing.Size(176, 23)
-        Me.Btn_Button.TabIndex = 1
-        Me.Btn_Button.Text = "This is a button"
-        Me.Btn_Button.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.Btn_Button)
-        Me.Controls.Add(Me.Btn_Trap)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Btn_Trap As Button
-    Friend WithEvents Btn_Button As Button
 End Class
--- Inventory Management System/Form1.vb
+++ Inventory Management System/Form1.vb
@@ -2,12 +2,4 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'This is a comment
     End Sub
-
-    Private Sub Btn_Button_Click(sender As Object, e As EventArgs) Handles Btn_Button.Click
-        MsgBox("Also a bit shit if im honest")
-    End Sub
-
-    Private Sub Btn_Trap_Click(sender As Object, e As EventArgs) Handles Btn_Trap.Click
-        MsgBox("Bit shit really")
-    End Sub
 End Class
