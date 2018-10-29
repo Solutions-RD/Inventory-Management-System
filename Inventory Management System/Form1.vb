4a7d422e182e614f8896cc9370516c6b1190e4b6
Daniel Langridge <daniel.langridge1@outlook.com>
2018-10-29 18:08:41 +0000

    Added btn's

--- Inventory Management System/Form1.vb
+++ Inventory Management System/Form1.vb
@@ -2,4 +2,12 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'This is a comment
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("This is a button")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("This is also a button")
+    End Sub
 End Class
