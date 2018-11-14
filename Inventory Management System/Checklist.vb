c5ecbe7071f53c8f90f9a6b223a47e6cb903548f
Daniel Langridge <daniel.langridge1@outlook.com>
2018-11-14 12:35:18 +0000

    Added Checklist.vb

---
+++ Inventory Management System/Checklist.vb
@@ -0,0 +1,26 @@
+﻿Public Class Checklist
+
+    'This class is being used purely as a "What shit needs to be done / added" textfile.
+    'Please ensure you comment anything you add, otherwuse you are gonna have a hard time running the program. We no likey Syntax Errors
+
+    'ChangeLoginDetails:
+
+
+    'Login:
+
+
+    'NonAdminCustomer:
+
+
+    'NonAdminCustomerFullStock:
+
+
+    'NonAdminUser:
+
+
+    'NonAdminUserLookUp:
+
+
+    'NonAdminUserSupplier:
+
+End Class
